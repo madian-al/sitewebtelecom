60e7b8a9a6547ca5cb0096ad4f81a49b166caf3a
madian-al <madian.alabbas@gmail.com>
2015-10-22 11:51:30 +0200

    Version vers 12H

---
+++ global.js
@@ -0,0 +1,26 @@
+// Custom JS
+$(document).ready(function(){
+  // Add your code here
+  // ------------------------------
+	// http://twitter.com/mattsince87
+	// ------------------------------
+
+	function scrollNav() {
+	  $('.nav a').click(function(){  
+		//Toggle Class
+		$(".active").removeClass("active");      
+		$(this).closest('li').addClass("active");
+		var theClass = $(this).attr("class");
+		$('.'+theClass).parent('li').addClass('active');
+		//Animate
+		$('html, body').stop().animate({
+			scrollTop: $( $(this).attr('href') ).offset().top - 55
+		}, 200);
+		return false;
+	  });
+	  $('.scrollTop a').scrollTop();
+	}
+
+
+	scrollNav();
+});
